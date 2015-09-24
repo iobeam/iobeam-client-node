87f719de97c6cccfb7fa71741ba2cdc1602cb149
Rob Kiefer <rob.kiefer@gmail.com>
2015-09-24 12:42:46 -0400

    resources/datapoint: spaces not tabs.

--- src/resources/Datapoint.js
+++ src/resources/Datapoint.js
@@ -1,14 +1,14 @@
 "use strict";
 
 function _DataPoint(value, timestamp) {
-	let ts = timestamp || null;
-	if (ts === null) {
-		ts = Date.now();
-	}
-	return {
-		value: value,
-		timestamp: ts
-	};
+    let ts = timestamp || null;
+    if (ts === null) {
+        ts = Date.now();
+    }
+    return {
+        value: value,
+        timestamp: ts
+    };
 }
 
 module.exports = _DataPoint;
