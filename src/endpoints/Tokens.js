52b03871290bff61524a8dae0f7ad4bb2b6f48eb
Rob Kiefer <rob.kiefer@gmail.com>
2015-10-10 21:07:42 -0400

    endpoints/Tokens: support for getting project token

--- src/endpoints/Tokens.js
+++ src/endpoints/Tokens.js
@@ -1,9 +1,16 @@
 "use strict";
 
+const ApiException = require("../exceptions/ApiException");
 const RequestResults = require("../constants/RequestResults");
 const Utils = require("../utils/Utils");
 
 let _requester = null;
+
+function assertValidPermissions(perms) {
+    if (perms === null || typeof(perms) !== "object") {
+        throw new ApiException("permissions must be an object");
+    }
+}
 
 module.exports = {
 
@@ -18,6 +25,43 @@ module.exports = {
         const authStr = "Basic " +
             new Buffer(username + ":" + password).toString("base64");
         req.set("Authorization", authStr);
+
+        const innerCb = (status, webResp) => {
+            if (status == RequestResults.PENDING) {
+                return;
+            } else if (!Utils.isCallback(callback)) {
+                return;
+            }
+
+            const resp = Utils.getDefaultApiResp(status, webResp);
+            if (!resp.timeout) {
+                const body = webResp.body;
+                if (status === RequestResults.SUCCESS) {
+                    resp.body = body;
+                } else if (status === RequestResults.FAILURE) {
+                    resp.error = body.errors[0].message;
+                }
+            }
+            callback(resp);
+        }
+        _requester.execute(req, innerCb);
+    },
+
+    getProjectToken: function(projectId, permissions, userToken, callback) {
+        Utils.assertValidProjectId(projectId);
+        Utils.assertValidToken(userToken);
+        assertValidPermissions(permissions);
+
+        const URL = _requester.getFullEndpoint("/tokens/project");
+        const req = _requester.getRequest(URL, userToken);
+
+        const readPerm = permissions.read || false;
+        const writePerm = permissions.write || false;
+        const adminPerm = permissions.admin || false;
+        req.query({read: readPerm, write: writePerm, admin: adminPerm})
+            .query({project_id: projectId})
+            .query({include_user: false});
+
 
         const innerCb = (status, webResp) => {
             if (status == RequestResults.PENDING) {
