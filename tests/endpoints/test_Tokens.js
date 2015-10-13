0a74d501dacdbd314a6318389cdf9aa0ea67f792
Rob Kiefer <rob.kiefer@gmail.com>
2015-10-12 21:21:06 -0400

    tests: add tests for getting project token.

--- tests/endpoints/test_Tokens.js
+++ tests/endpoints/test_Tokens.js
@@ -5,40 +5,106 @@ const RequestResults = require("../../src/constants/RequestResults");
 
 const CORRECT_USER = "test";
 const CORRECT_PASS = "correct";
+const CORRECT_PID = 1;
+const CORRECT_TOKEN = "dummytoken";
 
 const __req = {
+    url: null,
     headers: new Map(),
-    set: function(key, value) {
-        this.headers.set(key, value);
+    params: {},
+    set: (key, value) => {
+        __req.headers.set(key, value);
+        return __req;
+    },
+    query: (params) => {
+        Object.assign(__req.params, params);
+        return __req;
+    },
+    reset: () => {
+        __req.url = null;
+        __req.headers = new Map();
+        __req.params = {};
     }
 }
 
 const fakeRequester = {
-    execute: (r, cb) => {
-        let user = null;
-        let pass = null;
+
+    _handleGetUserToken: (r, cb) => {
         if (r.headers) {
             const auth = r.headers.get("Authorization");
             if (auth) {
                 const slice = auth.slice(auth.indexOf(" ") + 1);
                 const args = new Buffer(slice, "base64").toString("ascii").split(":");
                 if (args.length === 2 && args[0] === CORRECT_USER && args[1] === CORRECT_PASS) {
-                    cb(RequestResults.SUCCESS, {status: 200, body:{}});
+                    cb(RequestResults.SUCCESS,
+                        {
+                            status: 200,
+                            body: {}
+                        }
+                    );
                     return;
                 }
             }
         }
         const errors = [{message: "invalid"}];
-        cb(RequestResults.FORBIDDEN, {status: 403, body: {errors: errors}});
+        cb(RequestResults.FORBIDDEN,
+            {
+                status: 403,
+                body: {errors: errors}
+            }
+        );
     },
+
+    _handleGetProjectToken: (r, cb) => {
+        if (r.headers) {
+            const auth = r.headers.get("Authorization");
+            const correctToken = auth === "Bearer " + CORRECT_TOKEN;
+            const correctPid = r.params.project_id === CORRECT_PID;
+            if (correctToken && correctPid) {
+                cb(RequestResults.SUCCESS,
+                    {
+                        status: 200,
+                        body: {}
+                    }
+                );
+                return;
+            }
+        }
+        const errors = [{message: "invalid"}];
+        cb(RequestResults.FORBIDDEN,
+            {
+                status: 403,
+                body: {errors: errors}
+            }
+        );
+    },
+
+    execute: (r, cb) => {
+        if (r.url === "/tokens/user") {
+            fakeRequester._handleGetUserToken(r, cb);
+        } else if (r.url === "/tokens/project") {
+            fakeRequester._handleGetProjectToken(r, cb);
+        }
+    },
+
     getFullEndpoint: (e) => e,
-    getRequest: () => __req,
-    postRequest: () => { ; }
+    getRequest: (url, token) => {
+        __req.url = url;
+        if (token) {
+            __req.set("Authorization", "Bearer " + token);
+        }
+        return __req;
+    },
+    postRequest: () => { ; },
+    reset: () => {
+        __req.reset();
+    }
 }
 
 describe("test user token", () => {
     Tokens.initialize(fakeRequester);
     it("good combo returns body and success", () => {
+        fakeRequester.reset();
         const cb = (resp) => {
             expect(typeof(resp)).toBe("object");
             expect(resp.timeout).toBe(false);
@@ -68,7 +134,45 @@ describe("test user token", () => {
     };
     badCases.forEach((c) => {
         it(c.msg, () => {
+            fakeRequester.reset();
             Tokens.getUserToken(c.user, c.pass, badCb);
+        });
+    });
+});
+
+describe("test project token", () => {
+    Tokens.initialize(fakeRequester);
+    it("good combo returns body and success", () => {
+        fakeRequester.reset();
+        const cb = (resp) => {
+            expect(typeof(resp)).toBe("object");
+            expect(resp.timeout).toBe(false);
+            expect(resp.allowed).toBe(true);
+            expect(resp.code).toBe(200);
+            expect(resp.success).toBe(true);
+            expect(typeof(resp.body)).toBe("object");
+        };
+        const perms = {read: true};
+        Tokens.getProjectToken(CORRECT_PID, perms, CORRECT_TOKEN, cb);
+    });
+
+    const badCases = [
+        {msg: "wrong pid -> 403/error", pid: 2, token: CORRECT_TOKEN},
+        {msg: "wrong token -> 403/error", pid: CORRECT_PID, token: "WRONG"}
+    ];
+    const badCb = (resp) => {
+        expect(typeof(resp)).toBe("object");
+        expect(resp.timeout).toBe(false);
+        expect(resp.allowed).toBe(false);
+        expect(resp.code).toBe(403);
+        expect(resp.success).toBe(false);
+        expect(typeof(resp.error)).toBe("object");
+    };
+    badCases.forEach((c) => {
+        it(c.msg, () => {
+            fakeRequester.reset();
+            const perms = {read: true};
+            Tokens.getProjectToken(c.pid, perms, c.token, badCb);
         });
     });
 });
