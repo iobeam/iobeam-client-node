ee4563ea80986f520bdef7fd414e0bf4c8df08b5
Rob Kiefer <rob.kiefer@gmail.com>
2015-10-21 14:59:22 -0400

    endpoints: Learn to talk to /exports endpoint

---
+++ src/endpoints/Exports.js
@@ -0,0 +1,45 @@
+"use strict";
+const RequestResults = require("../constants/RequestResults");
+const Utils = require("../utils/Utils");
+
+let _token = null;
+let _requester = null;
+
+module.exports = {
+
+    initialize: function(token, requester) {
+        Utils.assertValidToken(token);
+        Utils.assertValidRequester(requester);
+        _token = token;
+        _requester = requester;
+    },
+
+    query: function(projectId, deviceId, seriesName, options, callback) {
+        Utils.assertValidToken(_token);
+        Utils.assertValidProjectId(projectId);
+        const did = deviceId || "all";
+        const sname = seriesName || "all";
+        if (did != "all") {
+            assertValidDeviceId(did);
+        }
+        const endpoint = "/exports/" + projectId + "/" + did + "/" + sname;
+        const URL = _requester.getFullEndpoint(endpoint);
+        const context = {
+            projectId: projectId,
+            deviceId: deviceId,
+            seriesName: seriesName,
+            options: options
+        };
+
+        const req = _requester.getRequest(URL, _token);
+        const bodyHandler = function(resp, body, status) {
+            if (status == RequestResults.SUCCESS) {
+                resp.body = body;
+            } else if (status == RequestResults.FAILURE) {
+                resp.error = body.errors[0].message;
+            }
+        };
+        const innerCb = Utils.createInnerCb(callback, context, bodyHandler);
+        _requester.execute(req, innerCb);
+    }
+};
