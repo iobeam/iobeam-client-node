fc07a77acadd720add5f72a5182f6f3565cff943
Rob Kiefer <rob.kiefer@gmail.com>
2016-03-30 15:25:08 -0400

    resources: Turn DataStore into a 'named' type to make instanceof work.

--- src/resources/DataStore.js
+++ src/resources/DataStore.js
@@ -21,101 +21,103 @@ const _DataStore = function(fields, rows) {
     const _fields = fields;
     const _rows = rows;
 
-    const ret = {
-
-        /**
-         * Add a new row to the store at a particular time.
-         * @param {int} time - Timestamp for all the points
-         * @param {object} data - Represents data to add for this row, with
-         * the property names representing the fields. Omitted field names are
-         * set to null.
-         */
-        add: function(time, data) {
-            if (this.size() + fields.length > 500) {
-                throw new Exception("batches are limited to 500 data points");
-            }
-
-            if (data === null || typeof(data) === "undefined") {
-                throw new Exception("data cannot be null/undefined");
-            }
-
-            // Verify data object
-            for (let k in data) {
-                if (!Utils.isInArray(k, _fields)) {
-                    throw new Exception("data contains field not in this batch: " + k);
-                }
-            }
-            let row = {time: time};
-
-            _fields.forEach(function(f) {
-                const val = data[f] || null;
-                row[f] = val;
-            });
-            _rows.push(row);
-        },
-
-        /**
-         * Add a new row to the store at the current timestamp.
-         * @param {object} data - Represents data to add for this row, with
-         * the property names representing the fields. Omitted field names are
-         * set to null.
-         */
-        addNow: null,  // Defined below
-
-        /* Return the list of fields in this store */
-        fields: function() {
-            return _fields.slice(0);
-        },
-
-        /* Return the list of rows in this store */
-        rows: function() {
-            const ret = [];
-            _rows.forEach(function(r) {
-                const temp = {};
-                Object.keys(r).forEach(function(k) {
-                    temp[k] = r[k];
-                });
-                ret.push(temp);
-            });
-            return ret;
-        },
-
-        reset: function() {
-            _rows.splice(0, _rows.length);
-        },
-
-        /* Return the table size (rows * fields) of this store */
-        size: function() {
-            return _fields.length * _rows.length;
-        },
-
-        /* Return a new DataStore with the content of this one at the current state */
-        snapshot: function() {
-            return new _DataStore(this.fields(), this.rows());
+    /**
+     * Add a new row to the store at a particular time.
+     * @param {int} time - Timestamp for all the points
+     * @param {object} data - Represents data to add for this row, with
+     * the property names representing the fields. Omitted field names are
+     * set to null.
+    */
+    this.add = function(time, data) {
+        if (this.size() + fields.length > 500) {
+            throw new Exception("batches are limited to 500 data points");
         }
+
+        if (data === null || typeof(data) === "undefined") {
+            throw new Exception("data cannot be null/undefined");
+        }
+
+        // Verify data object
+        for (let k in data) {
+            if (!Utils.isInArray(k, _fields)) {
+                throw new Exception("data contains field not in this batch: " + k);
+            }
+        }
+        let row = {time: time};
+
+        _fields.forEach(function(f) {
+            const val = data[f] || null;
+            row[f] = val;
+        });
+        _rows.push(row);
     };
 
-    ret.addNow = function(data) {
-        ret.add(Date.now(), data);
+    /**
+     * Add a new row to the store at the current timestamp.
+     * @param {object} data - Represents data to add for this row, with
+     * the property names representing the fields. Omitted field names are
+     * set to null.
+     */
+    this.addNow = function(data) {
+        this.add(Date.now(), data);
     };
 
-    return ret;
+    /* Get the list of fields in this store. */
+    this.fields = function() {
+        return _fields.slice(0);
+    };
+
+    /* Get the list of rows in this store */
+    this.rows = function() {
+        const ret = [];
+        _rows.forEach(function(r) {
+            const temp = {};
+            Object.keys(r).forEach(function(k) {
+                temp[k] = r[k];
+            });
+            ret.push(temp);
+        });
+        return ret;
+    };
+
+    this.reset = function() {
+        _rows.splice(0, _rows.length);
+    };
+
+    /**
+     * Get the size of this store.
+     * @returns {int} Size of the store, which is (# of fields) * (# of rows).
+     */
+    this.size = function() {
+        return _fields.length * _rows.length;
+    };
+
+    /**
+     * Get a new copy of this DataStore at the current state.
+     * @returns {DataStore} A copy of the current DataStore with the same
+     * fields and rows.
+     */
+    this.snapshot = function() {
+        return new DataStore(this);
+    };
 };
 
 /**
  * Represents a data store, i.e., rows of data points.
  * @constructor
- * @param {array} fields - List of columns in this batch
- * @returns {object} Object representing a batch of data in table format.
+ * @param {array} fields - List of columns in this store.
+ * @returns {DataStore} Object representing a store of data in table format.
  */
 function DataStore(fields) {
-
-    if (fields === null || typeof(fields) === "undefined" ||
-        fields.constructor !== Array) {
-        throw new Exception("fields must be an array of strings");
+    if (fields === null || typeof(fields) === "undefined") {
+        throw new Exception("Cannot make DataStore from null/undefined");
+    } else if (fields instanceof DataStore) {
+        _DataStore.call(this, fields.fields(), fields.rows());
+    } else if (fields.constructor === Array) {
+        _DataStore.call(this, fields.slice(0), []);
+    } else {
+        throw new Exception("Unable to make DataStore with: " + fields);
     }
-
-    return new _DataStore(fields.slice(0), []);
 }
 
 module.exports = DataStore;
