bbd175fcfef25e6e221f69ff171a856dda537efa
Rob Kiefer <rob.kiefer@gmail.com>
2016-03-29 22:26:38 -0400

    deps: Update superagent to 1.8

--- tests/http/test_Requester.js
+++ tests/http/test_Requester.js
@@ -5,7 +5,7 @@ const RequestResults = require("../../src/constants/RequestResults");
 
 const TEST_URL = "test";
 const TEST_TOKEN = "dummy";
-
+const AUTH_HEADER = "Authorization";
 
 describe("execute", () => {
     const cases = [
@@ -89,11 +89,11 @@ describe("getRequests", () => {
     it("gives a GET request w/ auth", () => {
         const req = Requester.getRequest(TEST_URL, TEST_TOKEN);
         expect(req.url).toBe(TEST_URL);
-        const headers = req.req._headers;
+        const headers = req.header;
         expect(typeof(headers)).toBe("object");
-        expect(headers.hasOwnProperty("authorization")).toBe(true);
+        expect(headers.hasOwnProperty(AUTH_HEADER)).toBe(true);
         const shouldToken = "Bearer " + TEST_TOKEN;
-        expect(headers.authorization).toBe(shouldToken);
+        expect(headers[AUTH_HEADER]).toBe(shouldToken);
     });
 });
 
@@ -103,24 +103,22 @@ describe("postRequests", () => {
     it("tests a POST request w/o a body", () => {
         const req = Requester.postRequest(TEST_URL);
         expect(req.url).toBe(TEST_URL);
-        expect(typeof(req.req)).toBe("object");
         expect(req._data).toBe(undefined);
     });
 
     it("gives a POST request w/o auth", () => {
         const req = Requester.postRequest(TEST_URL, testBody);
         expect(req.url).toBe(TEST_URL);
-        expect(typeof(req.req)).toBe("object");
         expect(req._data).toBe(testBody);
     });
 
     it("gives a POST request w/ auth", () => {
         const req = Requester.postRequest(TEST_URL, testBody, TEST_TOKEN);
         expect(req.url).toBe(TEST_URL);
-        const headers = req.req._headers;
+        const headers = req.header;
         expect(typeof(headers)).toBe("object");
-        expect(headers.hasOwnProperty("authorization")).toBe(true);
+        expect(headers.hasOwnProperty(AUTH_HEADER)).toBe(true);
         const shouldToken = "Bearer " + TEST_TOKEN;
-        expect(headers.authorization).toBe(shouldToken);
+        expect(headers[AUTH_HEADER]).toBe(shouldToken);
     });
 });
